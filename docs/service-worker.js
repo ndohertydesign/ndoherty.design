e4960ca13806fe7e260fe54ee6dbae6f7b9b3a6c
nicholasmdoherty <nicholasmdoherty@gmail.com>
2020-06-27 12:48:31 -0400

    deploy removal of shop link

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44febe0ae97c4e07d0d2819ee1693156.js"
+  "/precache-manifest.ff9668466cf447d98125a02c00d38597.js"
 );
 
 self.addEventListener('message', (event) => {
