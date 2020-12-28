939426dca8801b6c917b845e315246309946bdc2
nicholasmdoherty <nicholasmdoherty@gmail.com>
2020-12-27 19:35:32 -0500

    deploy project tags

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2bacd9dff3af686870b9393aedfb9d4.js"
+  "/precache-manifest.ed75e68034be289970350ec035cbacbf.js"
 );
 
 self.addEventListener('message', (event) => {
