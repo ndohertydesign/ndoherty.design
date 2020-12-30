4164c9ca26bfb8adc3b0280ee5b890583b0dc6ca
nicholasmdoherty <nicholasmdoherty@gmail.com>
2020-12-30 18:38:04 -0500

    deploy hosted PDF worker to fix mobile

---
+++ docs/precache-manifest.0088fc3fa1bd84f5dd1fc8dc6557fa1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a561b211cc8a42401a36038929ac8ee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0fedcae023d1e8a9fcc",
+    "url": "/static/css/main.b98f7024.chunk.css"
+  },
+  {
+    "revision": "a8f27945e3076e8e286a",
+    "url": "/static/js/0.215bd391.chunk.js"
+  },
+  {
+    "revision": "7a7d0fa1e9ad2232205a",
+    "url": "/static/js/3.59d3f3ef.chunk.js"
+  },
+  {
+    "revision": "fc0898d1c78357c1e02434f99d34e07a",
+    "url": "/static/js/3.59d3f3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d95aed2e15f753cafd3",
+    "url": "/static/js/4.3ee121b0.chunk.js"
+  },
+  {
+    "revision": "3aec583f49ba53228853",
+    "url": "/static/js/5.226c53cf.chunk.js"
+  },
+  {
+    "revision": "feea0e9056448aaa2406",
+    "url": "/static/js/6.59faafaf.chunk.js"
+  },
+  {
+    "revision": "d8ce4f2fcba17490b37a",
+    "url": "/static/js/7.22de9af6.chunk.js"
+  },
+  {
+    "revision": "42916acb0aba4ee618fe",
+    "url": "/static/js/8.f755a8e5.chunk.js"
+  },
+  {
+    "revision": "e0fedcae023d1e8a9fcc",
+    "url": "/static/js/main.a4255366.chunk.js"
+  },
+  {
+    "revision": "762bc536cfcce94d9ea9",
+    "url": "/static/js/runtime-main.1cbed21d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78640e4c22710aa0b1c1be7c7ddcd93a.js"
+  "/precache-manifest.0088fc3fa1bd84f5dd1fc8dc6557fa1c.js"
 );
 
 self.addEventListener('message', (event) => {
