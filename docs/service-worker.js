fdf0f16ce3988f5703a3c504c4d8394f2e880410
nicholasmdoherty <nicholasmdoherty@gmail.com>
2020-12-30 19:12:59 -0500

    deploy webpack worker entry for PDF

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1603f6bd15a77e0d163b30ef6480dc7d.js"
+  "/precache-manifest.e9ae76d64a6d94823496f38fac5ddc5f.js"
 );
 
 self.addEventListener('message', (event) => {
