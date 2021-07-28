76019385d340d9525d5cd2cd8bbbf2ecde79afd0
nicholasmdoherty <nicholasmdoherty@gmail.com>
2021-07-28 13:21:18 -0400

    deploy v2 redesign

---
+++ docs/precache-manifest.3419bdb89304789af862fa0cef2b430c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ab166108b1b6b14df4ab181d3b5d51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73799ca0063a47246cd4",
+    "url": "/static/css/main.59469b09.chunk.css"
+  },
+  {
+    "revision": "4755e4999d31baa31e1a",
+    "url": "/static/js/2.7cf2a18b.chunk.js"
+  },
+  {
+    "revision": "1818e4d9abf4a9c698bf3e11c4f1ac03",
+    "url": "/static/js/2.7cf2a18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73799ca0063a47246cd4",
+    "url": "/static/js/main.748da646.chunk.js"
+  },
+  {
+    "revision": "bd46c72df0d1cb95d69c",
+    "url": "/static/js/runtime-main.8167a49d.js"
+  }
+]);
